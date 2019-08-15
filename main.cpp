0b92de930036e01fb350911cd18c6edddf61f0a5
Lucas Eriksson <dracc@dracc.se>
2019-08-15 21:14:56 +0200

    main: Use new class names

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@
 int main() {
   doInit();
 
-  render Renderer;
+  Renderer renderer;
   
   // Do the shit
   SDL_GameController* p1gc = SDL_GameControllerOpen(0);
@@ -22,33 +22,33 @@ int main() {
   SDL_SetHint(SDL_HINT_JOYSTICK_ALLOW_BACKGROUND_EVENTS, "1");
 
   SDL_Rect playingField{10, 20, 620, 440};
-  player p1(20, p1gc, SDL_CONTROLLER_AXIS_LEFTY);
-  player p2(612, p1gc, SDL_CONTROLLER_AXIS_RIGHTY);
+  Player p1(20, p1gc, SDL_CONTROLLER_AXIS_LEFTY);
+  Player p2(612, p1gc, SDL_CONTROLLER_AXIS_RIGHTY);
 
   int p1Score = 3;
   int p2Score = 3;
 
-  ball b(playingField);
+  Ball b(playingField);
   
   while (1) {
-    Renderer.clear();
+    renderer.clear();
     p1.update(playingField);
     p2.update(playingField);
     b.update(p1, p2, playingField);
     if (p1Score != p1.getScore()) {
-      Renderer.updatePlayerScore(p1);
+      renderer.updatePlayerScore(p1);
       p1Score = p1.getScore();
     }
     if (p2Score != p2.getScore()) {
-      Renderer.updatePlayerScore(p2);
+      renderer.updatePlayerScore(p2);
       p2Score = p2.getScore();
     }
-    Renderer.drawField(playingField);
-    Renderer.drawScores(p1, p2);
-    Renderer.drawPlayer(p1);
-    Renderer.drawPlayer(p2);
-    Renderer.drawBall(b);
-    Renderer.flip();
+    renderer.drawField(playingField);
+    renderer.drawScores(p1, p2);
+    renderer.drawPlayer(p1);
+    renderer.drawPlayer(p2);
+    renderer.drawBall(b);
+    renderer.flip();
   }
 
   SDL_GameControllerClose(p1gc);
